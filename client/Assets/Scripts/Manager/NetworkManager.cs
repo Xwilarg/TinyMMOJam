d9baec8b634ffd4e3a679dfcb10c95e2378d9d0b
Xwilarg <xwilarg@protonmail.com>
2026-01-24 17:25:33 +0100

    Add Unity network

---
+++ client/Assets/Scripts/Manager/NetworkManager.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace MMOJam.Manager
+{
+    public class NetworkManager : MonoBehaviour
+    {
+        public static NetworkManager Instance { private set; get; }
+
+        private void Awake()
+        {
+            Instance = this;
+        }
+    }
+}
