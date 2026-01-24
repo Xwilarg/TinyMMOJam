95f7ccc2325a500a5082d211219535d2cfcc8592
Xwilarg <xwilarg@protonmail.com>
2026-01-24 14:11:07 +0100

    Add NetworkPlayerInstance

---
+++ client/Assets/Scripts/Player/NetworkPlayerInstance.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+namespace MMOJam.Player
+{
+    public class NetworkPlayerInstance : MonoBehaviour
+    {
+        private Rigidbody _rb;
+
+        private void Awake()
+        {
+            _rb = GetComponent<Rigidbody>();
+        }
+
+        public void UpdateVelocity(Vector3 adjustedPosition, Vector3 newVelocity)
+        {
+            _rb.linearVelocity = newVelocity;
+            transform.position = adjustedPosition;
+        }
+    }
+}
