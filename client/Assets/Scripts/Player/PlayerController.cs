5f44b96a1a25cce9ecb61b2708e1f9822cdb0db3
Xwilarg <xwilarg@protonmail.com>
2026-01-24 14:05:09 +0100

    Add player controller

---
+++ client/Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,63 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.Windows;
+
+namespace MMOJam.Player
+{
+    public class PlayerController : MonoBehaviour
+    {
+        [Header("Configuration")]
+        [SerializeField]
+        private float _mouvementSpeed = 5f;
+
+        [SerializeField]
+        private float _gravityMultiplier = .75f;
+
+        private CharacterController _controller;
+        private Vector2 _mov;
+        private float _verticalSpeed;
+
+        private void Awake()
+        {
+            _controller = GetComponent<CharacterController>();
+        }
+
+
+        protected virtual void Update()
+        {
+            if (!_controller.enabled)
+                return;
+
+            var pos = _mov;
+            Vector3 desiredMove = transform.forward * pos.y + transform.right * pos.x;
+
+            // Get a normal for the surface that is being touched to move along it
+            Physics.SphereCast(transform.position, _controller.radius, Vector3.down, out RaycastHit hitInfo,
+                               _controller.height / 2f, Physics.AllLayers, QueryTriggerInteraction.Ignore);
+            desiredMove = Vector3.ProjectOnPlane(desiredMove, hitInfo.normal).normalized;
+
+            Vector3 moveDir = Vector3.zero;
+            moveDir.x = desiredMove.x * _mouvementSpeed;
+            moveDir.z = desiredMove.z * _mouvementSpeed;
+
+            if (_controller.isGrounded && _verticalSpeed < 0f) // We are on the ground and not jumping
+            {
+                moveDir.y = -.1f; // Stick to the ground
+                _verticalSpeed = -_gravityMultiplier;
+            }
+            else
+            {
+                // We are currently jumping, reduce our jump velocity by gravity and apply it
+                _verticalSpeed += Physics.gravity.y * _gravityMultiplier * Time.deltaTime;
+                moveDir.y += _verticalSpeed;
+            }
+
+            _controller.Move(moveDir * Time.deltaTime);
+        }
+
+        public void OnMovement(InputAction.CallbackContext value)
+        {
+            _mov = value.ReadValue<Vector2>();
+        }
+    }
+}
