40683d6cc3468ba563b2888106c061845d69be00
TheIndra55 <theindra@protonmail.com>
2026-01-25 17:36:46 +0100

    Add zone detection

--- Assets/Scripts/Manager/ZoneManager.cs
+++ Assets/Scripts/Manager/ZoneManager.cs
@@ -1,11 +1,27 @@
+using Assets.Scripts.Zone;
+using System.Collections.Generic;
+using System.Linq;
 using Unity.Netcode;
+using UnityEngine;
 
 public class ZoneManager : NetworkBehaviour
 {
     public static ZoneManager Instance { private set; get; }
 
+    private readonly List<ZoneController> _zones = new();
+
     private void Awake()
     {
         Instance = this;
+    }
+
+    public void RegisterZone(ZoneController zone)
+    {
+        _zones.Add(zone);
+    }
+
+    public ZoneController GetZoneFromTransform(Transform transform)
+    {
+        return _zones.FirstOrDefault(zone => zone.Contains(transform));
     }
 }
--- Assets/Scripts/Zone/ZoneController.cs
+++ Assets/Scripts/Zone/ZoneController.cs
@@ -7,5 +7,24 @@ namespace Assets.Scripts.Zone
     {
         [SerializeField]
         private string _name;
+
+        private Collider _collider;
+
+        public string Name => _name;
+
+        private void Awake()
+        {
+            _collider = GetComponent<Collider>();
+        }
+
+        private void Start()
+        {
+            ZoneManager.Instance.RegisterZone(this);
+        }
+
+        public bool Contains(Transform transform)
+        {
+            return _collider.bounds.Contains(transform.position);
+        }
     }
 }
