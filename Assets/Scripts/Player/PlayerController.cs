078e95e2a6aecc4e33e4f0a7ddd79e4cd4891460
Xwilarg <xwilarg@protonmail.com>
2026-01-25 14:43:52 +0100

    Fix compile error

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -45,7 +45,6 @@ namespace MMOJam.Player
             if (IsHost || IsServer)
             {
                 var tArea = GetComponentInChildren<TriggerArea>();
-                tArea.
             }
         }
 
