32da84af17c69b692de4a1a2bbfb1f340e3f8495
Luxio-Chaosyn <107632641+Luxio-Chaosyn@users.noreply.github.com>
2026-01-25 15:39:18 +0100

    add ressourcesHolders code

--- Assets/Scripts/Manager/RessourcesManager.cs
+++ Assets/Scripts/Manager/RessourcesManager.cs
@@ -1,13 +1,16 @@
 using Unity.Netcode;
+using UnityEngine;
+using System.Collections.Generic;
 
 namespace MMOJam.Manager
 {
     public class RessourcesManager : NetworkBehaviour
     {
-        public static ServerManager Instance { private set; get; }
-        public void OnStartServer()
+        public static RessourcesManager Instance { private set; get; }
+        private Dictionary<EntityId, RessourcesHolder> _ressources_holders = new();
+        public void RegisterHolder(RessourcesHolder elem)
         {
-
+            _ressources_holders.Add(elem.gameObject.GetEntityId(), elem);
         }
     }
 }
