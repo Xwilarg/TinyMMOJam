bc71003c3a5951ba2dcd78bd39e334f2d38a02a0
Xwilarg <xwilarg@protonmail.com>
2026-01-25 13:42:09 +0100

    Add vehicle SO

---
+++ Assets/Scripts/SO/VehicleInfo.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+namespace MMOJam.SO
+{
+    [CreateAssetMenu(menuName = "ScriptableObject/VehicleInfo", fileName = "VehicleInfo")]
+    public class VehicleInfo : ScriptableObject
+    {
+        public string Name;
+        public SeatType[] Seats;
+    }
+
+    public enum SeatType
+    {
+        Driver,
+        Passenger,
+        Miner
+    }
+}
