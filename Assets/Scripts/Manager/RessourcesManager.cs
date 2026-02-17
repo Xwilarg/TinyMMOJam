bce909a68e69f8ca77437e6034c7d7777e01c443
Luxio-Chaosyn <107632641+Luxio-Chaosyn@users.noreply.github.com>
2026-02-18 00:39:20 +0100

    Update RessourcesManager.cs

--- Assets/Scripts/Manager/RessourcesManager.cs
+++ Assets/Scripts/Manager/RessourcesManager.cs
@@ -38,7 +38,7 @@ namespace MMOJam.Manager
         }
         public void RegisterHolder(RessourcesHolder elem)
         {
-            _ressources_holders.Add(elem.GetComponent<NetworkObject>().NetworkObjectId, elem);
+            _ressources_holders[elem.GetComponent<NetworkObject>().NetworkObjectId] = elem;
             Debug.Log($"[RHL] added {elem.GetComponent<NetworkObject>().NetworkObjectId} total [{string.Join(", ", _ressources_holders.Keys)}]");
         }
 
